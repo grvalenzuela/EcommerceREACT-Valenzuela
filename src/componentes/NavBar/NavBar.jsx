fea0d7b2497b5ec56acfdffa6c15ab1f2e9d0809
Gabriel Román Valenzuela <gabrielromanvlz@gmail.com>
2022-03-05 14:27:59 -0300

    Agrega bootstrap y genera navBar

---
+++ src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+//import NavDropdown from "react-bootstrap/NavDropdown";
+//import Widget from '../Widget/Widget'
+
+function NavBar() {
+  //console.log(props)
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">PC-Commerce</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#">Inicio</Nav.Link>
+              <Nav.Link href="#">Arma tu pc</Nav.Link>
+            </Nav>
+            {/* <Nav>
+              <Nav.Link eventKey={2} href="#memes">
+                <Widget />
+              </Nav.Link>
+            </Nav> */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
